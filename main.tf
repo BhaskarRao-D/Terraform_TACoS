991e7031a5eb1ae75fd0bc47e21e46f8d2203db0
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2025-07-21 11:47:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-<<<<<<< HEAD
-  profile = "default"
-}
-
-resource "aws_instance" "testing" {
-  ami           = "ami-09e6f87a47903347c"
-  instance_type = "t2.micro"
-=======
-}
 
 resource "aws_instance" "Testing_Automation" {
   ami           = "ami-0150ccaf51ab55a51"
   instance_type = "t2.small"
->>>>>>> master
 }
